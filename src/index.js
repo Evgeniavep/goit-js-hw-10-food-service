0852393032d9d2aedb2cba6c462024cc835f6d18
Evhenia Vepretskaya <enychka12@gmail.com>
2021-07-09 16:07:29 +0300

    Не работает ЛС

--- src/index.js
+++ src/index.js
@@ -16,48 +16,48 @@ listElement.insertAdjacentHTML('beforeend', tmpl(menu));
 
 themeToggle.addEventListener('click', onToggleClick);
 
-localStorage.setItem('theme', Theme.LIGHT);
+localStorage.setItem('bodyTheme', Theme.LIGHT);
 
 function onToggleClick(evt) {
   const checked = evt.currentTarget.checked;
   if (checked) {
     document.body.classList.add(Theme.DARK);
     document.body.classList.remove(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.DARK); 
+    localStorage.setItem('bodyTheme', Theme.DARK); 
   }
   else {
     document.body.classList.remove(Theme.DARK);
     document.body.classList.add(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.LIGHT);
+    localStorage.setItem('bodyTheme', Theme.LIGHT);
   }
 }
 
-  localStorage.getItem('theme')
-    if ('theme' === Theme.DARK) {
+  // localStorage.getItem('bodyTheme')
+  //   if ('bodyTheme' === Theme.DARK) {
+  //   document.body.classList.add(Theme.DARK);
+  //   document.body.classList.remove(Theme.LIGHT);
+  //   localStorage.setItem('bodyTheme', Theme.DARK);
+  //   themeToggle.checked = true;
+  //   };
+  //   if ('bodyTheme' === Theme.LIGHT) {
+  //   document.body.classList.add(Theme.LIGHT);
+  //   document.body.classList.remove(Theme.DARK);
+  //   };
+
+
+  checkThemeStat();
+function checkThemeStat() {
+  const currentThemeMod = localStorage.getItem('bodyTheme');
+  if (currentThemeMod === Theme.DARK) {
     document.body.classList.add(Theme.DARK);
     document.body.classList.remove(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.DARK);
+    localStorage.setItem('bodyTheme', Theme.DARK);
     themeToggle.checked = true;
-    };
-    if ('theme' === Theme.LIGHT) {
-    document.body.classList.add(Theme.LIGHT);
+  }
+  if (currentThemeMod === Theme.LIGHT) {
     document.body.classList.remove(Theme.DARK);
-    };
-
-
-
-
-
-
-
-
-// function reloadTheme() {
-//   localStorage.getItem('theme');
-//   if (reloadTheme === Theme.DARK) {
-//       document.body.classList.add(Theme.DARK);
-//       document.body.classList.remove(Theme.LIGHT);
-//       localStorage.setItem('theme', Theme.DARK);
-//       themeToggle.checked = true;
-//     }
-//   return;
-//   }
+    document.body.classList.add(Theme.LIGHT);
+    localStorage.setItem('bodyTheme', Theme.LIGHT);
+    themeToggle.checked = false;
+  }
+}
